922aa062338b92c341b8a3df2f067fff8693b524
Cesar Lopez <cesarl@outlook.com>
2016-05-07 15:38:25 -0600

    MyClient Created

---
+++ MyClient/MyClient/Default.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using MyClient.MyServiceReference;
+
+namespace MyClient
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            var client = new MyServiceClient();
+
+            var countries = client.CountriesGetAll();
+
+            lbCountries.DataSource = countries;
+            lbCountries.DataValueField = "Id";
+            lbCountries.DataTextField = "Name";
+            lbCountries.DataBind();
+        }
+    }
+}
