308746a53907448a9aa6ee3da0804d908fbed32d
Cesar Lopez <cesarl@outlook.com>
2016-05-07 09:31:03 -0600

    S01 L02 - Passing Parameters

--- S01/L1_TestService/IMyService.cs
+++ S01/L1_TestService/IMyService.cs
@@ -7,5 +7,17 @@ namespace L1_TestService
     {
         [OperationContract]
         string GetData();
+
+        [OperationContract]
+        string GetMessage(string Name);
+
+        [OperationContract]
+        string GetResult(int Sid, string SName, int M1, int M2, int M3);
+
+        [OperationContract]
+        int GetMax(int[] ar);
+
+        [OperationContract]
+        int[] GetSorted(int[] ar);
     }
 }
--- S01/L1_TestService/MyService.cs
+++ S01/L1_TestService/MyService.cs
@@ -1,10 +1,43 @@
-﻿namespace L1_TestService
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace L1_TestService
 {
     public class MyService: IMyService
     {
         public string GetData()
         {
             return "www.youtube.com";
+        }
+
+        public string GetMessage(string Name)
+        {
+            return "Hello Mr./Ms " + Name;
+        }
+
+        public string GetResult(int Sid, string SName, int M1, int M2, int M3)
+        {
+            double avg = (M1 + M2 + M3)/3.0;
+
+            if (avg < 35)
+                return "Fail";
+            else
+                return "Pass";
+
+        }
+
+        public int GetMax(int[] ar)
+        {
+            return ar.Max();
+        }
+
+        public int[] GetSorted(int[] ar)
+        {
+            Array.Sort(ar);
+
+            return ar;
+
         }
     }
 }
