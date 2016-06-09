001a239a8f85e6d00fdab248aa93ed6a3c1aa8bc
Cesar Lopez <cesarl@outlook.com>
2016-06-09 11:01:46 -0600

    UserName Based Custom Authentication with Basic Http binding

---
+++ WcfSecurity/WcfSecurityLib/CustomUserNamePasswordValidator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IdentityModel.Selectors;
+using System.IdentityModel.Tokens;
+
+namespace WcfSecurityLib
+{
+    public class CustomUserNamePasswordValidator : UserNamePasswordValidator
+    {
+        public override void Validate(string userName, string password)
+        {
+            if (userName != "test" || password != "test")
+            {
+                throw new SecurityTokenException("Access Denied");
+            }
+
+        }
+    }
+}
--- WcfSecurity/WebClient/Default.aspx.cs
+++ WcfSecurity/WebClient/Default.aspx.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using WcfSecurityServiceReference;
+using System.ServiceModel.Security;
 
 public partial class _Default : System.Web.UI.Page
 {
@@ -14,9 +15,23 @@ public partial class _Default : System.Web.UI.Page
     }
     protected void Button1_Click(object sender, EventArgs e)
     {
-        var cc = new CalculatorClient("BasicHttpBinding_ICalculator");
+        try
+        {
+            var cc = new CalculatorClient("BasicHttpBinding_ICalculator");
 
-        Response.Write(cc.Add(4, 96));
+            cc.ClientCredentials.UserName.UserName = "test";
+            cc.ClientCredentials.UserName.Password = "t78est";
+
+            Response.Write(cc.Add(4, 96));
+        }
+        catch(MessageSecurityException ex)
+        {
+            Response.Write("Access Denied");
+        }
+        catch(Exception ex)
+        {
+            Response.Write(ex.Message);
+        }
 
     }
 }
