6f2636c4bfa8c46ffe40f2a6b8dcab21fe416bc6
Cesar Lopez <cesarl@outlook.com>
2016-06-10 17:10:23 -0600

    Simple Asp.Net WebAPI Service

---
+++ MyWebAPI/MyWebAPI/Controllers/TestController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MyWebAPI.Controllers
+{
+    // ../api/Test
+    public class TestController : ApiController
+    {
+        string[] s = new string[] {"Cesar Lopez testing Web API ...", "Asp.Net WebAPI"};
+
+        // ../api/Test
+        public string[] Get()
+        {
+            return s;
+        }
+
+        // ../api/Test/id
+        public string Get(int id)
+        {
+            if (id >= 0 && s.Length > id)
+                return s[id];
+
+            return string.Empty;
+        }
+    }
+}
